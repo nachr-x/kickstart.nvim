5ad02a274ad8f4b30489b3c38d504cccadb9a074
nachr <nachr.dev@outlook.com>
2026-02-20 10:38:33 +0800

    通过统计数据、见解、历史记录和热图，可视化和查看您的 Neovim 配置活动

--- init.lua
+++ init.lua
@@ -255,6 +255,12 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
   --
+  {
+    "aikhe/wrapped.nvim",
+    dependencies = { "nvzone/volt" },
+    cmd = { "NvimWrapped" },
+    opts = {},
+  },
 
   {
     'stevearc/aerial.nvim',
